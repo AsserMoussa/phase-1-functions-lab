c6118e841727b8a70429339dd4aecc8467b17bd2
assermoussa <asser.y.moussa@gmail.com>
2024-01-18 21:41:27 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blocks; 
+    if (someValue > 41){
+    blocks = someValue - 42;
+    return blocks;}
+    else {
+        blocks = 42 - someValue; 
+        return blocks;
+    } 
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    let feet = 264 * distanceFromHqInBlocks(someValue);
+    return feet;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    if (destination > start)
+    {let dist = destination - start;
+    let total = dist * 264;
+    return total;}
+    else {
+        let dist = start - destination;
+    let total = dist * 264;
+    return total;
+    }
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let totalDist = distanceTravelledInFeet(start, destination);
+    if (totalDist<401){
+        return 0;
+    }
+    else if (totalDist>400 && totalDist<2001){
+        totalDist = totalDist - 400;
+        return totalDist*0.02;
+    }
+    else if (totalDist>2000 && totalDist<2501){
+        return 25;
+    }
+    else {return "cannot travel that far";}
+    }
